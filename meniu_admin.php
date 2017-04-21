1382a56a575f2c56e49a189703a54b57bf2fea29
lauraionita <lauraionita81@gmail.com>
2017-04-21 18:14:46 +0300

    meniu admin

---
+++ meniu_admin.php
@@ -0,0 +1,16 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>Search for imprisoned</title>
+</head>
+<body >
+<h2>Meniu Admin</h2>
+
+<p><a href="vizualizare_detinuti.php">Lista detinuti</a></p>
+<p><a href="optiuni_detinut.php">Optiuni detinuti</a><p>
+<p><a href="functii.php">Statistici</a><p>
+<p><a href="paginarea.php">Lista vizitelor</a><p>
+
+
+</body>
+</html>
