de0017d72c4776db4ae3fb62f4a09c031c722b68
anaspiridon <anamaria_spiridon@yahoo.com>
2017-04-25 00:18:14 +0300

    Pagina de "Acasa" pentru meniu user

--- meniu_user.php
+++ meniu_user.php
@@ -1,31 +1,107 @@
+<!DOCTYPE html>
+<html lang="ro">
 <html>
 <link href="meniu_user.css" type="text/css" rel="stylesheet">
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
   <title>DETON</title>
 </head>
-<body>
+<body bgcolor="#EEEEEE">
+
 <ul class="navigation">
-    <li class="nav-item"><a href="#">Acasa</a></li>
- 
-  
+   <li class="nav-item"><a href="meniu_user.php">Acasa</a></li>
     <li class="nav-item"><a href="#">Contact</a></li>
    <li class="nav-item"><a href="#">Programare online</a></li>
         <li class="nav-item"><a href="#">Acces penitenciar <span class="sub-navigation"></span></span></a>
-        <ul>
-
+         <ul>
             <li> <a href="#">Obiecte permise la vizita</a></li>
             <li> <a href="#">Obiecte nepermise la vizita </a></li>
-            
-        </ul>
-    </li>
- 
-    
+         </ul>   
+	<li class="nav-item"><a href="login.php">Log out</a></li>
 </ul>
 
 <input type="checkbox" id="nav-trigger" class="nav-trigger" />
 <label for="nav-trigger"></label>
-<div class="site-wrap">
+
+
+<div class="site-wrap" >
+
+
+<div>
+ <h1> Detention Monitoring Tool <h1>  
+ </div>
+ 
+   <div id="leftcolumn">
+    <h3>Sesizeaza o fapta de coruptie!</h3>
+  <br><br><br>
+         <form action="formular.php" method="post">
+  <label for="username">Nume:</label>
+ 
+  <input type="text" name="nume" value="" placeholder="Numele tau...">
+  <br>
+  <label for="username">Prenume:</label>
+  
+  <input type="text" name="prenume" value="" placeholder="Prenumele tau...">
+  <br>
+  <label for="username">Email:</label>
+  
+  <input type="text" name="email" value="" placeholder="Email tau...">
+  <br>
+  <label for="username">Telefon:</label>
+  
+  <input type="text" name="telefon" value="" placeholder="Nr tau de telefon...">
+  <br>
+  <label for="username">Adresa:</label>
+  
+  <input type="text" name="telefon" value="" placeholder="Adresa ta...">
+  <br>
+  <label for="username">Subiect:</label>
+  
+  <input type="text" name="telefon" value="" placeholder="Subiectul...">
+  <br>
+  <label for="username">Contint:</label>
+  
+  <input type="text" name="continut" value="" placeholder="Continut...">
+  <br>
+  <button class="button">Trimite</button>  
+  </form>
+  <br><br>
+   <img src="sesizare.png" alt="HTML5 Icon" style="width:428px;height:528px;"">
+  
+    </div>
+	
+	
+    <div id="rightcolumn">
+
+<div id="google_translate_element"></div>
+
+<script type="text/javascript">
+function googleTranslateElementInit() {
+  new google.translate.TranslateElement({pageLanguage: 'ro'}, 'google_translate_element');
+}
+</script>
+
+<script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>	
+   <br><br><br><br>
+   <h4>
+ <ul>
+  <li>Cum sesizați o faptă de corupție?</li><br>
+  <h5> Aveți posibilitatea să contribuiți la semnalarea unor fapte de corupție despre care aveți cunoștință și la tragerea la răspundere a persoanelor suspectate de comiterea lor. În acest scop, aveți la dispoziție formularul din stanga, pe care il puteți completa.</h5><br>
+  <li>Cum este protejat cel care denunță?</li><br>
+   <h5> Mituitorul sau cumpărătorul de influență nu se pedepsește dacă denunță autorității fapta, mai înainte ca organul de urmărire să fi fost sesizat pentru acea infracțiune. Banii, valorile sau orice alte bunuri se restituie persoanei care le-a dat, în acest caz (art. 255 alin. 3 și 5 din Codul penal, respectiv art. 6¹ alin 2 și 4 din Legea 78/2000).</h5><br>
+  <li>Fazele procesului penal</li><br>
+   <h5> Procesul penal este o activitate ce urmărește constatarea la timp și în mod concret a faptelor ce constituie infracțiuni, astfel încât persoana care a săvârșit o infracțiune să fie trasă la raspundere penală.
+
+Procesul penal are trei faze:
+<ul>
+<li> Urmărirea penală; are ca obiect strângerea probelor pentru trimiterea în judecată a inculpatului. Această activitate este efectuată de organele de cercetare penală și de către procuror. </li>
+<li> Judecata; începe o dată cu sesizarea instanței și continuă până la rămânerea definitivă a hotărârii de condamnare. Această activitate este efectuată de judecător. </li>
+<li> Punerea în executare a hotărârilor penale de condamnare rămase definitive.</li>
+</ul>
+</h5><br>
+</ul>
+   </h4>
+    </div>
 
 </div>
 </body>
