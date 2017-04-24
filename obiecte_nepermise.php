7a7f0d4cb94d572411cfe2f57d6510350aae87b4
lauraionita <lauraionita81@gmail.com>
2017-04-25 02:31:32 +0300

    Add files via upload
    tabelul si informatiile

---
+++ obiecte_nepermise.php
@@ -0,0 +1,119 @@
+<!DOCTYPE HTML PUBLIC ">
+<html>
+<link href="obiecte_nepermise.css" type="text/css" rel="stylesheet">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+  <title>DETON</title>
+</head>
+<body>
+<ul class="navigation">
+    <li class="nav-item"><a href="#">Acasa</a></li>
+ 
+  
+    <li class="nav-item"><a href="#">Contact</a></li>
+   <li class="nav-item"><a href="program_online.php">Programare online</a></li>
+        <li class="nav-item"><a href="#">Acces penitenciar <span class="sub-navigation"></span></span></a>
+        <ul>
+
+            <li> <a href="obiecte_permise.php">Obiecte permise la vizita</a></li>
+            <li> <a href="obiecte_nepermise.php">Obiecte nepermise la vizita </a></li>
+        </ul>
+        <li class="nav-item"><a href="login.php">Log out</a></li>
+    </li>
+ 
+    
+</ul>
+
+<input type="checkbox" id="nav-trigger" class="nav-trigger" />
+<label for="nav-trigger"></label>
+<div class="site-wrap">
+<h1 align="right"> Ce nu ai voie sa introduci intr-o vizita</h1><br><br><br>
+<h2>Detinutilor le este interzisa: <br></h2>
+<h3>
+<ol>
+   <li> Primirea de produse alimentare care, in vederea consumului, necesita incalzire, coacere, fierbere sau alte tratamente termice;</li>
+
+    <li> Cumpararea de produse alimentare usor alterabile ori care, in vederea consumului, necesita incalzire, coacere, fierbere sau alte tratamente termice, cu exceptia cafelei, ceaiului, laptelui si a supelor instant;</li>
+
+    <li> Primirea si cumpararea de lamai si derivate ale acestora. </li>
+</ol>
+   <p> Se interzice, de asemenea,  primirea bunurilor, obiectelor şi produselor prevazute de anexa de mai jos, precum si a celor a caror manipulare implica risc de deteriorare.</p></h3>
+
+
+<h2 align="center"> BUNURILE interzis a fi primite, cumparate, pastrate si folosite de catre detinuti: </h2>
+
+<table align="left">
+ <tr>
+    <th><h3>Nr</h3></th>
+    <th id="categorie"><h3>Categorie</h3></th>
+    <th id="bunuri"><h3>Denumirea bunurilor si obiectelor</h3></th>
+  </tr>
+
+<tr>
+<th>1</th>
+<th> Bunuri si obiecte care pot fi folosite pentru a vatama alti detinuti ori alte persoane sau care pot provoca distrugeri ale proprietatii </th> 
+<th>
+<ul>
+<li> aerosoli - produse ori ambalaje; </li>
+<li> cuţite ori alte arme albe, inclusiv cele folosite pentru practicarea artelor marţiale;  </li>
+<li> materiale pirotehnice.  </li>
+
+</ul>
+</th>
+</tr>
+
+<tr>
+<th>2</th>
+<th> Bunuri si obiecte care pot fi folosite pentru a inregistra sistemele ori activitatile in legatura cu asigurarea sigurantei penitenciarului </th> 
+<th>
+<ul>
+<li> camere video ori alte aparate electronice care pot înregistra sau stoca imagini în orice format; </li>
+<li> playere ori casetofoane, cu excepţia celor autorizate de prezentul regulament;  </li>
+<li> aparate de fotografiat ori echipament fotografic de orice fel.   </li>
+
+</ul>
+</th>
+</tr>
+
+
+
+<tr>
+<th>3</th>
+<th>Bunuri si obiecte care afecteaza ori interfereaza cu sistemul de utilitati al locului de detinere </th> 
+<th>
+<ul>
+<li> aparate electrice, cu exceptia celor autorizate de prezentul regulament; </li>
+<li> materiale ce pot fi utilizate pentru tatuare;   </li>
+<li> bani (monede si cupiuri);    </li>
+<li> simboluri aferente bandelor si gruparilor interlope si de crima organizata, indiferent de suportul de stocare;  </li>
+<li> rezistente electrice folosite ca sursa de caldura;   </li>
+<li> convectoare, incalzitoare electrice.    </li>
+
+
+</ul>
+</th>
+</tr>
+
+
+<tr>
+<th>4</th>
+<th>Altele </th> 
+<th>
+<ul>
+<li> lamai si derivate ale acestora; </li>
+<li> energizante si anabolizante, pentru cresterea masei musculare;  </li>
+<li> orice fel de magneti, inclusiv cei din compunerea unor tipuri de jocuri autorizate;   </li>
+<li> filme, materiale video, publicatii, fotografii, postere cu caracter rasist, xenofob, pornografic;  </li>
+<li> componente, subansamble, accesorii pentru dispozitive de comunicare la distanta.     </li>
+
+</ul>
+</th>
+</tr>
+
+</table>
+
+
+</div>
+</body>
+</html>
+
