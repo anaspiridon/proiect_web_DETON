b6ccd39d507be2d23abfb9eccf39165d615667bc
madalinanechita <mada.nechita23@gmail.com>
2017-06-17 01:15:10 +0300

    Add files via upload
    Structura html a paginii si formularul ce trebuie completat in vederea inserarii unui detinut.

---
+++ admin/insereaza_detinuti_admin.php
@@ -0,0 +1,85 @@
+<!DOCTYPE HTML PUBLIC ">
+<html>
+<link href="meniu_admin.css" type="text/css" rel="stylesheet">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+  <title>DETON</title>
+</head>
+<body bgcolor="#EEEEEE">
+
+<ul class="navigation">
+    <li class="nav-item"><a href="meniu_admin.php">Acasa</a></li>
+	  <li class="nav-item"><a href="vizualizare_detinuti.php">Lista detinuti</a></li>
+    <li class="nav-item"><a href="#">Optiuni detinuti <span class="sub-navigation"></span></span></a>
+        <ul> 
+            <li> <a href="sterge_detinuti.php">Sterge detinut</a></li>
+            <li> <a href="insereaza_detinut.php">Insereaza detinut </a></li>
+        </ul>
+    </li>
+    <li class="nav-item"><a href="date.php">Statistici</a></li>
+    <li class="nav-item"><a href="vizualizare_vizite.php">Lista Vizitelor</a></li>
+     <li class="nav-item"><a href="login.php">Log out</a></li>
+</ul>
+
+<input type="checkbox" id="nav-trigger" class="nav-trigger" />
+<label for="nav-trigger"></label>
+
+
+<div class="site-wrap" >
+ <h1> Detention Monitoring Tool </h1> 
+ <br>
+ <br>
+<?php
+
+session_start();
+function get_imput_admin($nume = '', $prenume = '', $nrDosar = '', $sPedeapsa = '' , $pedeapsa ='' ,$data_nastere='')
+{
+    echo <<<END
+  <form action="insereaza_detinut.php" method="post">
+ 
+  <div><p><label for="nume">Nume detinut:</label>
+     <input type="text" name="nume" value="$nume"></p><div>
+
+  <p><label for="prenume">Prenume detinut:</label>
+     <input type="text" name="prenume" value="$prenume"></p>
+
+  <p><label for="nrDosar">Nr. Dosar:</label>
+     <input type="text" name="nrDosar" value="$nrDosar"></p> 
+  
+  <p><label for="sPedeapsa">Start Pedeapsa:</label>
+        <input type="date"  name="sPedeapsa" id="data" value="$sPedeapsa"></p>
+  
+  <p><label for=data_nastere> Data nastere :</label>
+        <input type="date"  name="data_nastere" id="data" value="$data_nastere"></p>
+
+  <p><label for ="gen" > Sex: </label>
+        <select name="gen" id="gen">
+          <option value = M> M </option>
+          <option value =F >F </option>
+         </select> </p>
+
+  <p><label for="pedeapsa">Pedeapsa:</label>
+        <select name="pedeapsa" id="pedeapsa">
+          <option value= 1 > crima </option>
+          <option value= 2 > frauda </option>
+          <option value= 3 > furt </option>
+          <option value= 4 > viol </option>
+          <option value= 5 > santaj </option>
+          <option value= 6 > fals </option>
+          <option value= 7 > trafic </option>
+          <option value= 8 > posesie </option>
+          <option value= 9 > prostitutie </option>
+          <option value= 10 > profanare </option>
+        </select>  </p>
+
+    <p><button class="button">Insereaza</button> 
+    </p>
+    <br />
+    </div>
+  </form>
+  </p>
+END;
+}
+</div>
+</body>
+</html>
